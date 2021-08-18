87695278067bbe197ec33395bc82a585232c5e8c
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-17 20:13:37 -0400

    commit tree e2a909b7b90afdf3a4c67a06b2d1aa6a6ec1bbec

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((books) => renderBooks(books));
 }
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -14,4 +17,4 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+})
